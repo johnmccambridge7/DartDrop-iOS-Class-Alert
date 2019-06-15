7398c05f88bccf373917e7ad8b3da89cad3576d0
John MacDonald <johnmacdonald@HPDDA2A1.home>
2019-06-15 19:48:57 +0100

    changing classifier to depend on json

--- reviews.py
+++ reviews.py
@@ -1,6 +1,7 @@
 import requests
 import urllib
 import time
+import json
 from lxml import html
 from lxml.etree import tostring
 
@@ -10,10 +11,22 @@ PASSWORD = "Unity2013"
 LOGIN_URL = "https://www.layuplist.com/accounts/login/"
 URL = "https://www.layuplist.com/best?page="
 
-def main():
+SEARCH_URL = "https://www.layuplist.com/search?q="
+FILE_OUTPUT = "FALL_professor_reviews_full.txt"
+TERM_ID = "201909"
+COURSE_JSON = "courses/FALL_course_output.json"
 
+# example ITAL010
+# bug when fetching page with multiple search results
+# scan through and find table row with Offered 19F
+
+
+def main():
+    json_file = open(COURSE_JSON)
+    parsed_json = json.loads(json_file.read())
+    
     try:
-        open("professor_reviews_full.txt", "r")
+        open(FILE_OUTPUT, "r")
         print("File has already been generated!")
         return True
     except FileNotFoundError:
@@ -38,78 +51,74 @@ def main():
 
     # Perform login
     result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
-    addresses = []
 
     # Obtain all the course data
-
-    for i in range(1,44):
-
-        # Scrape url
-        result = session_requests.get(URL + str(i), headers = dict(referer = URL))
-        tree = html.fromstring(result.content)
-
-        print("Fetching partition: " + str(i) + "/44")
-
-        for link in tree.xpath("//a[starts-with(@href, '/course/')]"):
-            address = link.get("href")
-
-            if address not in addresses:
-                addresses.append(address)
+    # open json file and iterate over each file
 
     # For each course get the list of professors that teach the course
+    for crn in parsed_json:
+        course_data = parsed_json[crn]
 
-    for address in addresses:
-        # address for each course
-        url = "https://www.layuplist.com" + address
+        if course_data['Term'] == TERM_ID:
+            course_name = course_data['Subj'] + course_data['Num']
+            url = SEARCH_URL  + course_name
 
-        result = session_requests.get(url, headers = dict(referer = URL))
-        tree = html.fromstring(result.content)
+            result = session_requests.get(url, headers = dict(referer = URL))
+            tree = html.fromstring(result.content)
 
-        num_reviews = 0
+            num_reviews = 0
+            course_url_id = result.url.split("/")
 
-        print("Gathering data from: " + address)
+            if len(course_url_id) > 4: 
+                course_url_id = "/" + course_url_id[3] + "/" + course_url_id[4]
+            else:
+                course_url_id = 0
 
-        if address not in review_rankings:
-            review_rankings[address] = dict()
+            print("Gathering data from: " + url)
 
-        div_column = "//div[@class='col-md-12']"
+            if course_url_id != 0:
+                if course_url_id not in review_rankings:
+                    review_rankings[course_url_id] = dict()
 
-        # search for each professor
+                div_column = "//div[@class='col-md-12']"
 
-        for professor in tree.xpath(div_column + "//table[@class='table table-striped']//tbody//tr//td//a/text()"):
-            if ":" not in professor:
-                professor = professor.strip()
-                
-                # go to the professors review page
-                professor_reviews_url = "https://www.layuplist.com" + address + "/review_search?q=" + urllib.parse.quote(professor)
-
-                professor_review_page = session_requests.get(professor_reviews_url, headers = dict(referer = URL))
-                professor_tree = html.fromstring(professor_review_page.content)
-
-                for review in professor_tree.xpath(div_column + "//table[@class='table table-striped']//tbody//tr//td[@class='highlight-review']/text()"):
-                    clean_review = review.replace(":", "").strip()
+                # search for each professor
+                for professor in tree.xpath(div_column + "//table[@class='table table-striped']//tbody//tr//td//a/text()"):
+                    if ":" not in professor:
+                        professor = professor.strip()
                     
-                    if clean_review != "":
+                        # go to the professors review page
+                        professor_reviews_url = "https://www.layuplist.com" + course_url_id + "/review_search?q=" + urllib.parse.quote(professor)
 
-                        if professor not in review_rankings[address]:
-                            review_rankings[address][professor] = []
-                        
-                        # dictionary to a class a professor teaches which stores a tuple of good bad rating
-                        num_reviews += 1
-                        total_reviews += 1
+                        professor_review_page = session_requests.get(professor_reviews_url, headers = dict(referer = URL))
+                        professor_tree = html.fromstring(professor_review_page.content)
 
-                        review_rankings[address][professor].append(clean_review)
+                        for review in professor_tree.xpath(div_column + "//table[@class='table table-striped']//tbody//tr//td[@class='highlight-review']/text()"):
+                            clean_review = review.replace(":", "").strip()
+                            
+                            if clean_review != "":
 
-        print("\tCollected " + str(num_reviews) + " from " + address)
-        num_reviews = 0
+                                if professor not in review_rankings[course_url_id]:
+                                    review_rankings[course_url_id][professor] = []
+                                
+                                # dictionary to a class a professor teaches which stores a tuple of good bad rating
+                                num_reviews += 1
+                                total_reviews += 1
+
+                                review_rankings[course_url_id][professor].append(clean_review)
+                
+                print("\tCollected " + str(num_reviews) + " from " + result.url)
+                num_reviews = 0
+            else:
+                print("Failed to collect some information.\n")
 
     time.sleep(2)
 
     print("=======================")
     print("Total Reviews Collected: " + str(total_reviews))
     print("=======================")
-
-    review_file = open("professor_reviews_full.txt", "w")
+    
+    review_file = open(FILE_OUTPUT, "w")
 
     for course in review_rankings.keys():
         course_reviews = review_rankings[course]
